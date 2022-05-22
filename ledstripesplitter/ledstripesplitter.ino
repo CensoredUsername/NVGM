d6c71129459d791afb6666628b3b33bb757cba6f
CensoredUsername <cens.username@gmail.com>
2022-05-22 23:11:40 +0200

    ledstripesplitter working

--- ledstripesplitter/ledstripesplitter.ino
+++ ledstripesplitter/ledstripesplitter.ino
@@ -1,33 +1,55 @@
 #include <avr/io.h>
 
 // port index of the input pin being used
-#define INPUT_PORT 0
+#define INPUT_PORT PE
 
 // pin index of the input pin being used
-#define INPUT_PIN 1
+#define INPUT_PIN 2
 
 // amount of cycles that the input has to be low to register a reset condition. Supports values up to 1500.
 // should be at most 1000 (50us), and at least 8us
 #define RESET_CYCLES 600
 
 // storage for segment bytecode. Don't change this.
-#define MAX_SEGMENT_COUNT 255
+#define MAX_SEGMENT_COUNT 127
 uint8_t segment_bytecode[2 * MAX_SEGMENT_COUNT] = {0};
+
+void setup() {
+    configure_input();
+    configure_segment(0, PE, 1);
+    configure_segment(1, PE, 0);
+    configure_segment(2, PB, 1);
+    configure_segment(3, PB, 0);
+    configure_segment(4, PE, 3);
+    configure_segment(5, PA, 1);
+    configure_segment(6, PF, 4);
+    configure_segment(7, PB, 2);
+    configure_segment(8, PC, 6);
+    configure_segment(9, PF, 5);
+    configure_segment(10, PA, 0);
+    configure_segment(11, PC, 4);
+    configure_segment(12, PC, 5);
+}
+
+void loop() {
+    cli();
+    execute_bytecode(4, 4);
+}
 
 void configure_input() {
     // configure input pin as input, enable pullup so it doesn't float.
     PORT_t& port_ref = (&PORTA)[INPUT_PORT];
     port_ref.DIRCLR = 1 << INPUT_PIN;
-    (&port_ref.PIN0CTRL)[INPUT_PIN] = 0x80;
+    (&port_ref.PIN0CTRL)[INPUT_PIN] = 0x08;
 }
 
 void configure_segment(uint8_t index, uint8_t port, uint8_t pin) {
     // set bytecode entry.
-    segment_bytecode[index] = 0x07 + 0x20 * port;
-    segment_bytecode[index + 1] = 1 << pin;
+    segment_bytecode[index * 2] = 0x07 + 0x20 * port;
+    segment_bytecode[index * 2+ 1] = 1 << pin;
 
     // pin init. set direction and initialize low.
-    PORT_t& port_ref = (&PORTA)[index];
+    PORT_t& port_ref = (&PORTA)[port];
     port_ref.DIRSET = 1 << pin;
     port_ref.OUTCLR = 1 << pin;
 }
@@ -45,10 +67,11 @@ void execute_bytecode(uint8_t segment_count, uint8_t segment_length) {
         "ldi r27, %[port_reg_hibyte] \n" // X high byte, high byte of all port register addresses
 
         // reset await loop prologue
+        "restart_reset_loop: \n"
         "ldi r18, %[reset_loop_cycles] \n"
         "reset_loop: \n" // this takes 6 cycles to loop, which at 20MHz means ~0.3us per loop
             "sbic %[input_port], %[input_pin] \n" // 1 cycle + 2 if next is skipped
-            "rjmp entry_point \n" // 2 cycles
+            "rjmp restart_reset_loop \n" // 2 cycles
             "dec r18 \n" // 1 cycle
             "brne reset_loop \n" // 2 cycles if taken, 1 if not
 
@@ -70,6 +93,7 @@ void execute_bytecode(uint8_t segment_count, uint8_t segment_length) {
 
                 // rising edge detected, toggle current output pin
                 "st X, r0 \n" // 1 cycle
+                "nop \n"
 
                 "falling_edge: \n"
                     "sbic %[input_port], %[input_pin] \n" // 1 cycle, + 2 if next skipped
@@ -93,18 +117,10 @@ void execute_bytecode(uint8_t segment_count, uint8_t segment_length) {
         , [bitcount_per_segment]"r"(24*(uint16_t)segment_length)
         , [reset_loop_cycles]"M"(RESET_CYCLES / 6)
         , [bytecode_addr]"r"((uint8_t *)segment_bytecode)
-        , [input_port]"I"(INPUT_PORT)
+        , [input_port]"I"(INPUT_PORT * 4 + 2)
         , [input_pin]"I"(INPUT_PIN)
         , [port_reg_hibyte]"I"(0x04)
         // clobbers
         : "r0", "r18", "r24", "r25", "r26", "r27", "r30", "r31"
     );
-}
-
-void setup() {
-    configure_input();
-}
-
-void loop() {
-    execute_bytecode(0, 16);
 }
---
+++ ledstripesplitter/ledstripetester.ino
@@ -0,0 +1,97 @@
+#include <avr/io.h>
+
+#define OUTPUT_PORT PE
+#define OUTPUT_PIN 2
+
+#define BYTECODE_LEN 3 * 16
+uint8_t bytecode[BYTECODE_LEN] = {
+    0xFF, 0xFF, 0xFF,
+    0x00, 0x00, 0x00,
+    0xAA, 0xAA, 0xAA,
+    0xFF, 0x00, 0xAA,
+    0xFF, 0xFF, 0xFF,
+    0x00, 0x00, 0x00,
+    0xAA, 0xAA, 0xAA,
+    0xFF, 0x00, 0xAA,
+    0xFF, 0xFF, 0xFF,
+    0x00, 0x00, 0x00,
+    0xAA, 0xAA, 0xAA,
+    0xFF, 0x00, 0xAA,
+    0xFF, 0xFF, 0xFF,
+    0x00, 0x00, 0x00,
+    0xAA, 0xAA, 0xAA,
+    0xFF, 0x00, 0xAA
+};
+
+// generates a waveform compatible with Ws2812 control
+
+void setup() {
+    // initialize output pin
+    PORT_t& port_ref = (&PORTA)[OUTPUT_PORT];
+    port_ref.DIRSET = 1 << OUTPUT_PIN;
+    port_ref.OUTCLR = 1 << OUTPUT_PIN;
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    cli();
+    for(;;) {
+        wait(50);
+        generate_signal(bytecode, BYTECODE_LEN);
+    }
+}
+
+void wait(uint8_t us) {
+    asm volatile (
+        "loop: \n"
+            "nop \n nop \n nop \n nop \n nop\n"
+            "nop \n nop \n nop \n nop \n nop \n"
+            "nop \n nop \n nop \n nop \n nop \n"
+            "nop \n nop \n"
+            "dec %[count] \n"
+            "brne loop \n"
+            "nop \n"
+        : [count]"+r"(us)
+        :
+        :
+    );
+}
+
+void generate_signal(uint8_t *bytecode, uint8_t bytecode_len) {
+    bytecode_len++;
+    asm volatile (
+        "rjmp entry_point \n"
+        "wave_start_early: \n"
+            "nop \n nop \n nop \n nop \n nop \n nop \n nop \n" // 7 cycles delay
+        "wave_start: \n"
+            "lsl r0 \n" // 1 cycle
+            "sbi %[output_port], %[output_pin] \n" // 1 cycle
+            "nop \n" // 1 cycle
+        "brcs one \n" // 2 cycles if taken, 1 if not
+            "nop \n nop \n nop \n nop \n" // 4 cycle delay
+            "cbi %[output_port], %[output_pin] \n" // 1 cycle delay
+            "nop \n nop \n" // 2 cycle delay
+            "rjmp wave_end \n" // 2 cycle delay
+        "one: \n"
+            "nop \n nop \n nop \n nop \n nop \n" // 5 cycle delay
+            "nop \n nop \n nop \n nop \n nop \n" // 5 cycle delay
+            "cbi %[output_port], %[output_pin] \n" // 1 cycle delay
+        "wave_end: \n"
+        // from this point onwards, need 0.50 ns delay (10 cycles) till back to wave_start
+            "dec r18 \n" // 1 cycles
+            "brne wave_start_early \n" // 2 cycles if taken, 1 if not.
+        "entry_point: \n"
+            "dec %[bytecode_remaining] \n" // 1 cycle
+            "breq done \n" // 2 cycles if taken, 1 if not.
+            "ld r0, %a[bytecode]+ \n" // 2 cycles
+            "ldi r18, 8 \n" // 1 cycle
+            "nop \n" // 1 cycle
+            "rjmp wave_start \n" // 2 cycles
+        "done: \n"
+        : [bytecode]"+e"(bytecode)
+        , [bytecode_remaining]"+r"(bytecode_len)
+        : [output_port]"I"(OUTPUT_PORT * 4 + 1)
+        , [output_pin]"I"(OUTPUT_PIN)
+        : "r0", "r18"
+    );
+}
